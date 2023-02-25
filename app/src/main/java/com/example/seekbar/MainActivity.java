9c79555f33788117d6c42e01cf9134c2e6cef91c
Deepa <114289926+deepakushwaha17@users.noreply.github.com>
2023-02-25 13:00:48 +0530

    Seek bar

--- app/src/main/java/com/example/seekbar/MainActivity.java
+++ app/src/main/java/com/example/seekbar/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     SeekBar seekBar;
 
-    @Override
+    @Override 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
